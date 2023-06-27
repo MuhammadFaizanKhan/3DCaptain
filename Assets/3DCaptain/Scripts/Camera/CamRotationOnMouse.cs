ed3bb60f618e9d11048ab2176341d598e2c52095
Muhammad Faizan Khan <5569155+MuhammadFaizanKhan@users.noreply.github.com>
2023-06-27 18:25:55 +0500

    Initial UI and camera added
    Top bar UI added with two cameras.
    One Know but is that on E/Q press camera keep moving above/down

---
+++ Assets/3DCaptain/Scripts/Camera/CamRotationOnMouse.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace ThreeDWalker
+{
+    /// <summary>
+    /// Function: This script will use to Rotate the camera using mouse
+    /// Author: Muhammad Faizan Khan
+    /// </summary>
+    public class CamRotationOnMouse : MonoBehaviour
+    {
+        /// <summary>
+        /// current camera
+        /// </summary>
+        private Transform camera;
+
+        /// <summary>
+        /// Rotation around the vertical axis is called yaw
+        /// </summary>
+        float yaw = 0.0f;
+
+        /// <summary>
+        /// Rotation around the side-to-side axis is called pitch
+        /// </summary>
+        float pitch = 0.0f;
+
+        public float rotationSpeed = 500f;
+
+        void Start()
+        {
+            camera = this.transform;//cashed current gameobject
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButton(1)) //left click detect and use cam rotation on mouse
+            {
+                yaw += Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
+                pitch -= Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSpeed;
+                camera.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+            }
+        }
+
+
+    }
+}
---
+++ Assets/3DCaptain/Scripts/Camera/CamWalk.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using ThreeDWalker;
+
+namespace ThreeDWalker
+{
+    /// <summary>
+    /// Function: Handle Cam Walk
+    /// Author: Muhammad Faizan Khan
+    /// </summary>
+    [RequireComponent(typeof(MovementInput))]
+    public class CamWalk : MonoBehaviour
+    {
+        [Header("Cam Walk Settings")]
+        public float speed = 10;
+
+        private MovementInput objMovementInput;
+        private GameObject camWalk;
+
+        void Start()
+        {
+            objMovementInput = GetComponent<MovementInput>();
+            camWalk = this.gameObject;
+        }
+
+        void Update()
+        {
+            var hr = objMovementInput.inputHorizontal* speed * Time.deltaTime; 
+            var vr = objMovementInput.inputVertical* speed * Time.deltaTime;
+
+            camWalk.transform.Translate(hr, 0, vr);
+        }
+    }
+}
